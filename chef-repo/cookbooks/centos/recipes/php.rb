e24364ef9efbe236b47b6e75c0edb4d2483eb364
Kenji Hara <haracane@gmail.com>
2012-12-29 21:21:41 +0900

    Apache+PHP設定レシピを追加しました

--- chef-repo/cookbooks/centos/recipes/php.rb
+++ chef-repo/cookbooks/centos/recipes/php.rb
@@ -28,16 +28,3 @@ execute "config /etc/php.ini" do
   EOF
 end
 
-[
-  "etc/httpd/conf.d/php.conf"
-  ].each do |filename|
-  filepath = "/#{filename}"
-  template filepath do
-    source "#{filename}.erb"
-  end
-  file filepath do
-    owner "root"
-    group "root"
-    mode  "0644"
-  end
-end
