b605cf2c43048509b566741eea9c866a311f4851
Kenji Hara <haracane@gmail.com>
2013-02-07 19:28:12 +0900

    Fix for Chef11

--- chef-repo/cookbooks/centos/recipes/attributes-hadoop-standalone.rb
+++ chef-repo/cookbooks/centos/recipes/attributes-hadoop-standalone.rb
@@ -1,22 +1,29 @@
+processor_count = `cat /proc/cpuinfo | grep processor | wc -l`.chomp.to_i
+
 node.set["variables"] = {
   "hadoop_config" => {
-    "masters"=>[node[:hostname]],
-    "slaves"=>[node[:hostname]],
+    "masters" => [node[:hostname]],
+    "slaves" => [node[:hostname]],
     "core-site.xml" => {
-      "fs.default.name"=>"hdfs://#{node[:hostname]}:8020",
-      "hadoop.tmp.dir"=>"/var/lib/hadoop-0.20/cache/${user.name}",
+      "fs.default.name" => "hdfs://#{node[:hostname]}:8020",
+      "webinterface.private.actions" => "true",
+      "hadoop.tmp.dir" => "/var/lib/hadoop-0.20/cache/${user.name}",
+      "io.compression.codecs" => "org.apache.hadoop.io.compress.GzipCodec,org.apache.hadoop.io.compress.DefaultCodec,org.apache.hadoop.io.compress.BZip2Codec"
     },
     "hdfs-site.xml" => {
-      "dfs.replication" => 2,
-      "dfs.name.dir" => "/var/lib/hadoop-0.20/cache/name",
-      "dfs.data.dir" => "/var/lib/hadoop-0.20/cache/${user.name}/dfs/data"
+      "dfs.name.dir" => "${hadoop.tmp.dir}/dfs/name",
+      "dfs.data.dir" => "${hadoop.tmp.dir}/dfs/data",
+      "dfs.replication" => 3,
+      "dfs.permissions" => "false",
+      "dfs.datanode.max.xcievers" => 2048      
     },
     "mapred-site.xml" => {
       "mapred.job.tracker" => "#{node[:hostname]}:8021",
       "mapred.map.tasks.speculative.execution" => "false",
-      "mapred.tasktracker.map.tasks.maximum" => node[:cpu].size,
-      "mapred.tasktracker.reduce.tasks.maximum" => node[:cpu].size,
-      "mapred.userlog.retain.hours" => 1
+      "mapred.tasktracker.map.tasks.maximum" => processor_count * 3 / 2,
+      "mapred.tasktracker.reduce.tasks.maximum" => processor_count,
+      "mapred.userlog.retain.hours" => 1,
+      "mapred.task.timeout" => 1800000
     }
   }
 }
